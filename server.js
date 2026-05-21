42852544ebdce14048845e282608c9518af8a8b8
oh jiseong <j1s3on9@gmail.com>
2026-05-21 20:41:41 +0900

    v3.3.2 patch release - miner bug fixes

--- server.js
+++ server.js
@@ -295,6 +295,12 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/register.js", (req, res) => {
+  res.type("application/javascript").sendFile(path.join(__dirname, "public", "Register.js"));
+});
+app.get("/Register.js", (req, res) => {
+  res.type("application/javascript").sendFile(path.join(__dirname, "public", "Register.js"));
+});
 
 let totalRequests = 0;
 let todayRequests = 0;
